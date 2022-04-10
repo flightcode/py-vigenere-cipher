f938e19cb104c63b28f16a50ad786b6c68611495
flightcode <flightcode@pm.me>
2022-04-10 14:05:46 +1000

    Add CLI argument option

--- vigenere.py
+++ vigenere.py
@@ -2,23 +2,81 @@
 # github.com/flightcode
 # 2022
 
-import time
+import sys # For CLI rguments
+import getopt # Parses CLI arguments
 
 def main(): # Welcome message and run menu
     print("--- VIGENERE CIPHER ----")
     print("--- FLIGHTCODE 2022 ----")
-    time.sleep(1)
-    menu()
 
-def decrypt(encrypted): # Decrypt string
-    print(f"Encrypted: {encrypted}")
-    decrypted = encrypted
-    print(f"Decrypted: {decrypted}")
+    fileName = "" # File to parse
+    mode = "" # Mode (interactive is for testing or if user wants a menu)
+    key = "" # If applicable, if not provided for decode, will attempt to crack. Required for encode.
 
-def encrypt(decrypted): # Encrypt string
-    print(f"Decrypted: {decrypted}")
-    encrypted = decrypted
-    print(f"Encrypted: {encrypted}")
+    try: # Attempt to parse arguments (Strip first argument (Command))
+        opts, args = getopt.getopt(sys.argv[1:], "m:f:k:")
+    except:
+        errorMessage("Unable to parse arguments!")
+
+    for opt, arg in opts: # Assign from arguments 
+        if opt in ['-f']:
+            fileName = arg
+        elif opt in ['-m']:
+            mode = arg
+        elif opt in ['-k']:
+            key = arg
+
+    if mode == "interactive":
+        menu()
+    elif mode == "encrypt" or mode == "decrypt":
+        if fileName != "":
+            file = open(fileName, "r") # Open file in read mode
+            decrypted = file.read() # Read file contents to string
+            file.close() # Close file
+            if mode == "encrypt":
+                if key != "":
+                    encrypt(decrypted,key,f"{fileName}.out") # Encrypt string `decrypted` with key `key` into file `[filename].out`
+                else:
+                    errorMessage("Key not specified!")
+            elif mode == "decrypt":
+                decrypt(encrypted,f"{fileName}.out")
+        else:
+            errorMessage("File not specified!")
+    else:
+        errorMessage("Invalid mode specified!")
+
+def encrypt(decrypted, key, output=""): # Encrypt string (OPTIONAL output filename param, outputs to CLI if empty)
+    print("--- START ENCRYPTION ---")
+    if output == "":
+        print(f"Decrypted: {decrypted}")
+        print(f"Key: {key}")
+    
+    encrypted = decrypted # Do work
+
+    if output == "":
+        print(f"Encrypted: {encrypted}")
+    else:
+        print(f"--- OUTPUT at '{output}' ---")
+        file = open(output, "w") # Open file in write mode
+        file.write(encrypted) # Write encrypted string to file contents
+        file.close() # Close file
+    print("--- FINISH ENCRYPTION ---")
+
+def decrypt(encrypted, output=""): # Decrypt string (OPTIONAL output filename param, outputs to CLI if empty)
+    print("--- START DECRYPTION ---")
+    if output == "":
+        print(f"Encrypted: {encrypted}")
+
+    decrypted = encrypted # Do work
+
+    if output == "":
+        print(f"Decrypted: {decrypted}")
+    else:
+        print(f"--- OUTPUT at '{output}' ---")
+        file = open(output, "w") # Open file in write mode
+        file.write(decrypted) # Write decrypted string to file contents
+        file.close() # Close file
+    print("--- FINISH DECRYPTION ---")
 
 def errorMessage(message): # Print param 'message' formatted as ERROR
     print("--- ERROR ---")
