e11952121141690d9dbefa6f720843223f8cfa2b
flightcode <flightcode@pm.me>
2022-04-10 13:14:29 +1000

    Add documentation

--- vigenere.py
+++ vigenere.py
@@ -4,30 +4,30 @@
 
 import time
 
-def main():
+def main(): # Welcome message and run menu
     print("--- VIGENERE CIPHER ----")
     print("--- FLIGHTCODE 2022 ----")
     time.sleep(1)
     menu()
 
-def decrypt(encrypted):
+def decrypt(encrypted): # Decrypt string
     print(f"Encrypted: {encrypted}")
     decrypted = encrypted
     print(f"Decrypted: {decrypted}")
 
-def encrypt(decrypted):
+def encrypt(decrypted): # Encrypt string
     print(f"Decrypted: {decrypted}")
     encrypted = decrypted
     print(f"Encrypted: {encrypted}")
 
-def errorMessage(message):
+def errorMessage(message): # Print param 'message' formatted as ERROR
     print("--- ERROR ---")
     print(f"{message}")
     print("--- ERROR ---")
 
-def menu():
+def menu(): # Menu Options
     menuLoop = True
-    while menuLoop:
+    while menuLoop: # Run until user selects to exit
         print("")
         print("--- MENU ---")
         print("1. Decrypt")
@@ -35,17 +35,17 @@ def menu():
         print("3. Exit")
         print("--- MENU ---")
         print("")
-        option = int(input("Enter option: "))
+        option = int(input("Enter option: ")) # Get input as int
         print("")
-        if option == 1:
+        if option == 1: # Decrypt
             print("--- STARTING DECRYPT ---")
             decrypt("test")
             print("--- FINISHED DECRYPT ---")
-        elif option == 2:
+        elif option == 2: # Encrypt
             print("--- STARTING ENCRYPT ---")
             encrypt("test")
             print("--- FINISHED ENCRYPT ---")
-        elif option == 3:
+        elif option == 3: # Exit
             print("--- GOODBYE ---")
             menuLoop = False
         else:
